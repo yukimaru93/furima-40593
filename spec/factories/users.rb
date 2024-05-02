41184a878fc36b65fc710a0aac38445bcd02d802
yukimaru93 <yuki15.jarvis@outlook.jp>
2024-05-02 14:49:51 +0900

    #4 ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Internet.username}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name {"亜あア"}
+    first_name {"亜あア"}
+    last_name_kana {"ア"}
+    first_name_kana {"ア"}
+    birth_day {Faker::Date.between(from: '1930-01-01', to: '2019-12-31')}
+  end
+end
