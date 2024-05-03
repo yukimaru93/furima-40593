eaace6aa924829c98c5b29a1af7b39cbe94164a5
yukimaru93 <yuki15.jarvis@outlook.jp>
2024-05-03 20:50:55 +0900

    #9 商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!,except: [:index, :show]
-    before_action :set_item, only: [:show, :edit, :update]
+    before_action :set_item, only: [:show, :edit, :update,:destroy]
     before_action :move_to_index, only: [:edit, :update]
 
     def index
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
             redirect_to item_path
         else
             render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if user_signed_in?
+            @item.destroy
+            redirect_to items_path
         end
     end
 
