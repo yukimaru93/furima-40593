91d93ed886b139f02800f59a2e3131dd917a02b9
yukimaru93 <yuki15.jarvis@outlook.jp>
2024-05-07 12:30:59 +0900

    関数の簡略化

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -1,19 +1,22 @@
 function calc() {
     const price = document.getElementById("item-price");
     price.addEventListener("keyup",() => {
-        const priceValue = price.value;  
-        const tax = document.getElementById("add-tax-price");
-        const profit = document.getElementById("profit");
-        let tax_calc = priceValue * 0.1;     
-        let profit_calc = priceValue - tax_calc;     
-
-        tax.innerHTML = Math.floor(tax_calc)       
-        profit.innerHTML = Math.floor(profit_calc)    
-
+        priceIndex(price);
     });
-    
-    
+    priceIndex(price);
 };
+
+const priceIndex = (price) => {
+    const priceValue = price.value;  
+    const tax = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    let tax_calc = priceValue * 0.1;     
+    let profit_calc = priceValue - tax_calc;     
+
+    tax.innerHTML = Math.floor(tax_calc)       
+    profit.innerHTML = Math.floor(profit_calc)    
+}
+
 
 
 window.addEventListener("turbo:load",calc);  
